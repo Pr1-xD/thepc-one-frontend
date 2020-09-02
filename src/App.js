ce5cd73c2a41cf2d0d50611c256cca579142a644
Pr1-xD <63440566+Pr1-xD@users.noreply.github.com>
2020-09-02 23:15:25 +0530

    Intial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home'
+import About from './About'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+      <Route path = "/home" component = {Home} />
+      <Route path = "/about" component = {About} />
+      </Switch>
     </div>
   );
 }
