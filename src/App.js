a27f264a40c5988a28f08f00a7d4f85334d43a22
Kush Dang <kushdang@gmail.com>
2020-09-28 14:32:34 +0530

    Worked on Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
 import Home from './Home/Home'
 import Events from './Events/Events'
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Switch>
       <Route path = "/home" component = {Home} />
       <Route path = "/events" component = {Events} />
