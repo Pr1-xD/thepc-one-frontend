ecf4dd32d9423984f60bc0eeabacb739c40df634
Kush Dang <kushdang@gmail.com>
2020-09-30 21:44:49 +0530

    Mapping cards

--- src/Eventshome.js
+++ src/Eventshome.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React , {useState,useEffect} from 'react'
 import axios from 'axios'
+import Card from "./Card/Card";
+import "./Eventshome.css"
 
-function Eventshome(){
+function Eventshome(props){
+
+    
+
+    const [eventsList, setEventsList] = useState(null);
+
+
 
     axios.get('https://thepc-one.herokuapp.com/api/allEvents')
     .then((response) => {
-        console.log(response);
+        console.log(response.data);
+        setEventsList(response.data);
+        console.log(eventsList);
     }, (error) => {
         console.log(error);
     });
     
 
+
     return(
-        <>
-        Card
-        </>
+        <div className="eventsHomeCard">
+            <div className="events_title"><h1>Events</h1></div>
+            {eventsList.map ((event) => {
+                return (
+                    <Card title={event.eventName}/>
+                )
+            })} 
+        </div>
     )
 }
 
