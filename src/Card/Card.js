94a17d66599dfc6c39bc5fc7ab74c5cecd67d86c
Pr1-xD <63440566+Pr1-xD@users.noreply.github.com>
2020-11-10 20:11:30 +0530

    Removed extra

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -3,15 +3,13 @@ import "./Card.css"
 
 function Card(props) {
     const userID=props.userID
-    // const [buttonText,setButtonText]=useState(props.buttonText)
-    // console.log(buttonText)
     const buttonText=props.buttonText
     const date=props.startDate
     const newDate = date.toString().substring(0,10);
 
     function eventsRegister()
     {   if(userID)
-       { // setButtonText('Registered')    
+       {     
         props.eventsRegister(
             {   
                 'eventName':props.name,
@@ -44,22 +42,6 @@ function Card(props) {
             </div>
         </div>
 
-
-
-
-        // <div>   
-        //     <div className="card_contentBox card card_box mb-3">
-        //         <img className="card_img" src="https://picsum.photos/seed/picsum/200/300" alt="Event-img" />
-        //         <div className="card_body card-body">
-        //             <h5 className="card_title card-title">{props.name}</h5>
-        //             <p className="card_bodyText card-text">{props.desc}</p>
-        //             <div className="card_bodyFooter card-text"><b>DATE: <span className="card_footerColor">{newDate}</span></b>     &emsp;  &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; <b>TIME: <span className="card_footerColor">2-6 PM</span></b>
-        //               {(props.buttonText)&&(props.buttonText!='Registered')?<button type="button" onClick={eventsRegister} className="btn btn-primary card_button">{props.buttonText}</button>:<></>}
-        //               {props.buttonText=='Registered'?<button type="button" className="btn btn-primary card_button_disabled">{props.buttonText}</button>:<></>}
-        //             </div>          
-        //         </div>
-        //     </div>
-        // </div>
     )
 }
 
