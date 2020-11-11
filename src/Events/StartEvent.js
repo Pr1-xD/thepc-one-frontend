1f7c8ac9d07e8731c599249f3b142e0b4fd84dc7
Pr1-xD <63440566+Pr1-xD@users.noreply.github.com>
2020-11-11 18:20:04 +0530

    Event

--- src/Events/StartEvent.js
+++ src/Events/StartEvent.js
@@ -5,8 +5,21 @@ function StartEvent(props){
     console.log(eventData)
     return(
         <>
-            <h1 className="events-header">{eventData}</h1>
+            <h1 className="events-header">{eventData.eventName}</h1>
         </>
     )
 }
 export default StartEvent
+
+// approved: true
+// dateCreated: "2020-09-30T13:47:36.717Z"
+// eventDesc: "This is event one I think"
+// eventEnd: "2020-10-05T19:43:01.654Z"
+// eventLink: "https://google.com"
+// eventName: "Event One"
+// eventStart: "2020-10-05T19:43:01.654Z"
+// isFileUpload: true
+// isMultiChoice: false
+// numFileUploads: 2
+// numMultiChoice: 0
+// numTextBoxes: 0
