4017866ac8fd92831071dbb4df0e3e77ea73ac05
Pr1-xD <63440566+Pr1-xD@users.noreply.github.com>
2020-11-13 00:18:55 +0530

    Event Page

--- src/Events/StartEvent.js
+++ src/Events/StartEvent.js
@@ -1,12 +1,24 @@
 import React, { useState } from 'react'
 
 function StartEvent(props){
+    const numTextBoxes=props.eventData.numTextBoxes
     const eventData=props.eventData
     console.log(eventData)
+    function TextField(i){
+        return(
+           
+            <input type="text" className="form-control" placeholder="Event" required />
+            
+           
+        )
+    }
     return(
-        <>
+        eventData?<div>
             <h1 className="events-header">{eventData.eventName}</h1>
-        </>
+            {Array.from(Array(10).keys()).map(TextField)}
+            {eventData.eventLink?<button className="button btn btn-lg btn-primary mt-5 custom" type="button">Go to Event</button>:<></>}
+            </div>
+            :<></>
     )
 }
 export default StartEvent
@@ -23,3 +35,4 @@ export default StartEvent
 // numFileUploads: 2
 // numMultiChoice: 0
 // numTextBoxes: 0
+// onChange={e=>abc.eventName=e.target.value}
